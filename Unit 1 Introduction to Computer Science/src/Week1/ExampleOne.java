1843eb5aa24995bcfc7c916365561b049d0d3f41
Romina Bahadorkhan <rbahadorkhan@Zbayviewglen.ca>
2021-09-09 13:26:11 -0400

    Initial Commit Unit One

---
+++ Unit 1 Introduction to Computer Science/src/Week1/ExampleOne.java
@@ -0,0 +1,17 @@
+package Week1;
+
+/**
+ * ExampleOne
+ */
+public class ExampleOne {
+    public static void main(String[] args) {
+        System.out.println("Welcome to IC3U - AP!");  // System.out.println displays text to screen and hits enter
+        System.out.println("Welcome to IC3U - AP!");
+        System.out.println("Welcome to IC3U - AP!");
+        // Anything in quotes in cosidered a String Literal
+        // A string is a sequence of characters (strings are in double Q
+    }
+    
+
+    
+}
---
+++ Unit 1 Introduction to Computer Science/src/Week1/ExampleTwo.java
@@ -0,0 +1,11 @@
+package Week1;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("A"); // prints A and hits enter
+        System.out.println("B"); // prints B and hits enter
+        System.out.println("C"); // prints C and hits enter
+        System.out.println("D"); // prints D and hits enter  
+        System.out.println("E"); // prints E and hits enter
+}
+}
