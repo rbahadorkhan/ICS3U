c175d6d715d6f61140b2244d0de000dc4e74627e
Romina Bahadorkhan <rbahadorkhan@Zbayviewglen.ca>
2021-09-13 13:01:39 -0400

    Final Modifier and Type Casting

---
+++ Unit 1 Introduction to Computer Science/src/week2/ExampleSeven.java
@@ -0,0 +1,23 @@
+package week2;
+
+/**
+ * Final Modifier - modifies  the variable so that becomes a constant
+ */
+public class ExampleSeven {
+    public static void main(String[] args) {
+        final int x=7; 
+        // x = 8;  // cannot change a final variable
+
+        final int y;
+        y = 8;
+        // y = 2; // cannot change a final variable
+
+        final int NUMBER_OF_STUDENTS = 13;  // naming convention for constants is UPPER_CASE, "_" for spaces
+
+
+
+
+        
+    }
+    
+}
