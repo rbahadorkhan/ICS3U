a39c40aca7571f000a721a308002b81b0b608009
Romina Bahadorkhan <rbahadorkhan@Zbayviewglen.ca>
2021-09-14 13:33:45 -0400

    Final Modifier and Type Casting

---
+++ Unit 1 Introduction to Computer Science/src/week2/ModulusExample.java
@@ -0,0 +1,17 @@
+package week2;
+
+public class ModulusExample {
+    public static void main(String[] args) {
+        int num = 4698;
+
+        int num1= num / 1000; // 4 
+        int num4 = num % 10; // 9
+        int num2 = num / 100 % 10; // 6
+        int num3 = num / 10 % 10; // 8 
+        //num4 = num % 100 / 10;
+
+
+        
+    }
+    
+}
