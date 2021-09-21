05293dd15b8ccab75e2282e3c180968fedd2aa4d
Romina Bahadorkhan <rbahadorkhan@Zbayviewglen.ca>
2021-09-21 13:27:24 -0400

    Strings

---
+++ Unit 2 - Using Objects/src/Week2/StringExamples.java
@@ -0,0 +1,16 @@
+package Week2;
+
+public class StringExamples {
+    public static void main(String[] args) {
+        String s1 = "This is a String Literal";
+        String s2 = "This is a String Literal";
+        String s3 = new String("This is a String Literal"); // it is not a string literal but a string object
+
+        System.out.println(s1.length());
+
+        System.out.println(s1.equals(s2));
+
+        
+    }
+    
+}
