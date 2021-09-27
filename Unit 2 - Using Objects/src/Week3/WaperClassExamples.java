7ac11ac2539cc91eb58b24d92bb441b9fec65eb4
Romina Bahadorkhan <rbahadorkhan@Zbayviewglen.ca>
2021-09-27 13:12:09 -0400

    Strings, Warpper classes and Math classes

---
+++ Unit 2 - Using Objects/src/Week3/WaperClassExamples.java
@@ -0,0 +1,23 @@
+package Week3;
+
+public class WaperClaaExamples {
+    public static void main(String[] args) {
+        Interger x = new Integer(7); // explicitly constructing an Interger object
+        Double d = new Double(3.4);
+
+        Double num = 3.4; // autoboxing (Java creates a Doyble object before assignment)
+
+        double dd1 = num; 
+        
+        System.out.println(num.doubleValue());
+        System.out.println(num);
+
+        System.out.println(x);
+        System.out.println(x.);
+
+        double 
+
+
+    }
+    
+}
