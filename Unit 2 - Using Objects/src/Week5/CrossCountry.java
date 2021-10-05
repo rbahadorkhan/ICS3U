f5fd22de2e733d63a5e5b2a7ded2d370a3bdc7ef
Romina Bahadorkhan <rbahadorkhan@Zbayviewglen.ca>
2021-10-05 12:54:07 -0400

    Cross Country Assignment

--- Unit 2 - Using Objects/src/Week5/CrossCountry.java
+++ Unit 2 - Using Objects/src/Week5/CrossCountry.java
@@ -1,48 +1,48 @@
 package Week5;
 
-public class CrossCountry {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        processRunner(in);
-        processRunner(in);
-        processRunner(in);
-        in.close();
+    import java.util.Scanner;
+
+    public class CrossCouuntry {
+       public static void main(String[] args) {
+          Scanner in = new Scanner(System.in);
+          processRunner(in);
+          processRunner(in);
+          processRunner(in);
+          in.close();
+       }
     
-
-        
-    }
-    private static void processRunner() {
-        String firstName, lastName
-        String mileOne, mileTwo, finishTime; 
-        String splitTwo, splitThree;
-        
-        /**
-         * get the name of mileOne, mileTwo, finishTime from the user
-         * 
-         */
-        
-         splitTwo = subtractTimes(mileTwo, mileOne);
-         splitThree = subtractTimes(finishTime, mileTwo);
-   
-         /**
-          * Display a summary for the runner
-          */
-      }   
-
-}
-    private static String subtractTimes(String endTime, String startTime ) {
-        double endInSeconds = convertToSeconds(endTime);
-        double startInSeconds = convertToSeconds(startTime);
-        double diffInSeconds = endInSeconds = startInSeconds; 
-        return convertToTime(diffInSeconds);
-
-
-        
-    }
-    private static String convertToTime(double diffInSeconds) {
-
-    ]
-    }
-    private static double convertToSeconds(String endTime) {
-        
+       private static void processRunner(Scanner in) {
+          String firstname, lastName;
+          String mileOne, mileTwo, finishTime;
+          String splitTwo, splitThree;
+    
+          /**
+           * get the name and mleOne, mileTwo, finishTime from the user
+           */
+    
+          splitTwo = subtractTimes(mileTwo, mileOne);
+          splitThree = subtractTimes(finishTime, mileTwo);
+    
+          /**
+           * Display a summary for the runner
+           */
+       }
+    
+       private static String subtractTimes(String endTime, String startTime) {
+          double endInSeconds = convertToSeconds(endTime);
+          double startInSeconds = convertToSeconds(startTime);
+    
+          double diffInSeconds = endInSeconds - startInSeconds;
+    
+          return convertToTime(diffInSeconds);
+       }
+    
+       private static String convertToTime(double diffInSeconds) {
+          return null;
+    
+          // create getMinutes and getSeconds functions to use
+       }
+    
+       private static double convertToSeconds(String endTime) {
+          return 0;
     }
