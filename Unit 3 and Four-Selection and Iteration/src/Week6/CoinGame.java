1e9483840265fd8b271458ece17735b2340f9386
Romina Bahadorkhan <rbahadorkhan@Zbayviewglen.ca>
2021-10-18 12:28:43 -0400

    Loops

---
+++ Unit 3 and Four-Selection and Iteration/src/Week6/CoinGame.java
@@ -0,0 +1,67 @@
+
+public class CoinGame {
+
+   private int startingCoins; // starting number of coins
+   private int maxRounds; // maximum number of rounds played
+
+   public CoinGame(int s, int r) {
+      startingCoins = s;
+      maxRounds = r;
+   }
+
+   /**
+    * Returns the number of coins (1, 2, or 3) that player 1 will spend.
+    */
+   public int getPlayer1Move() {
+      return 0;
+   }
+
+   /**
+    * Returns the number of coins (1, 2, or 3) that player 2 will spend, as
+    * described in part (a).
+    */
+   public int getPlayer2Move(int round) {
+      if (round % 3 == 0)
+         return 3;
+      else if (round % 2 == 0)
+         return 2;
+      else
+         return 1;
+   }
+
+   /**
+    * Plays a simulated game between two players, as described in part (b).
+    * 
+    */
+   public void playGame() {
+
+      int p1Coins = startingCoins;
+      int p2Coins = startingCoins;
+
+      int round = 1;
+
+      while (round <= maxRounds && p1Coins >= 3 && p2Coins >= 3) {
+         int p1Move = getPlayer1Move();
+         int p2Move = getPlayer2Move(round);
+
+         p1Coins -= p1Move;
+         p2Coins -= p2Move;
+
+         // logic
+         if (p1Move == p2Move)
+            p2Coins++;
+         else if (Math.abs(p1Move - p2Move) == 1)
+            p2Coins++;
+         else if (Math.abs(p1Move - p2Move) == 2)
+            p1Coins += 2;
+      }
+
+      if (p1Coins == p2Coins)
+         System.out.println("TIE");
+      else if (p1Coins > p2Coins)
+         System.out.println("P1 WINS");
+      else if (p1Coins > p2Coins)
+         System.out.println("P2 WINS");
+   }
+
+}
--- Unit 3 and Four-Selection and Iteration/src/Week6/WhileLoops.java
+++ Unit 3 and Four-Selection and Iteration/src/Week6/WhileLoops.java
@@ -1,47 +1,49 @@
 public class WhileLoops {
     public static void main(String[] args) {
-        exampleOne();
-        exampleTwo();
+        // exampleOne();
+        // exampleTwo();
         int numVowels = countVowels("This is a sentence with vowels");
         System.out.println(numVowels);
-        
-        
-    }
+
+   }
 
     private static int countVowels(String str) {
-        int numVowels= 0;
-        String vowels = "AEIOUaeiou";
-        int index = 0;
+      int numVowels = 0;
+      String vowels = "AEIOUaeiou";
+      int index = 0;
 
-        while(index <str.length()) {
-            String nextChar = str.substring(index, index + 1);
-            if (vowels.indexOf(nextChar)> 0 )
+      while (index < str.length()) {
+         String nextChar = str.substring(index, index + 1);
+         if (vowels.indexOf(nextChar) >= 0)
             numVowels++;
-            index++;
+         index++;
         }
+
+      return numVowels;
+
     }
 
     private static void exampleTwo() {
-        // add the number from 1- 100
-        
-        int sum = 0;
-        int numAdd = 1;
+      // add the numbers from 1 - 100 inclusive
+      int sum = 0;
+      int num = 1;
 
-        while(numAdd <= 100){
-            numAdd += sum;
-            numAdd++;
-     }
+      while (num <= 100) {
+         sum += num;
+         num++;
+        }
+
+      System.out.println(sum);
     }
 
     private static void exampleOne() {
-        //purpose of a loop is to repeat code
-        
-        int num = 1;
+      // purpose of a loop is to repeat code
 
-        while(num < 10){
-            System.out.println(num);
-            num++;
+      int num = 1;
+
+      while (num < 10) {
+         System.out.println(num);
+         num++;
         }
     }
-    
 }
