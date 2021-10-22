9b29c5dd6ebaee44a00feb6e1c1a647b556af973
Romina Bahadorkhan <rbahadorkhan@Zbayviewglen.ca>
2021-10-22 13:28:24 -0400

    Crazy Eights

--- Unit 3 and Four-Selection and Iteration/src/week7/CrazyEights.java
+++ Unit 3 and Four-Selection and Iteration/src/week7/CrazyEights.java
@@ -10,51 +10,113 @@ public class CrazyEights {
     private static final String QUEEN = "Q";
     private static final String KING = "K";
     public static void main(String[] args) {
-        String playerHand = "";
-        playerHand = getCard(playerHand) + " ";
-        playerHand += getCard(playerHand) + " ";
-        playerHand += getCard(playerHand) + " ";
-        playerHand += getCard(playerHand) + " ";
-        playerHand += getCard(playerHand) + " ";
-  
-        System.out.println(playerHand);
+      String playerHand = "";
+      playerHand = getCard(playerHand) + " ";
+      playerHand += getCard(playerHand) + " ";
+      playerHand += getCard(playerHand) + " ";
+      playerHand += getCard(playerHand) + " ";
+      playerHand += getCard(playerHand) + " ";
+      
+      System.out.println(playerHand);
+
+      String compOneHand = "";
+      compOneHand = getCard(compOneHand) + " ";
+      compOneHand+= getCard(compOneHand) + " ";
+      compOneHand += getCard(compOneHand) + " ";
+      compOneHand += getCard(compOneHand) + " ";
+      compOneHand += getCard(compOneHand) + " ";
+      System.out.println(compOneHand);
+
+      String compTwoHand = "";
+      compTwoHand = getCard(compTwoHand) + " ";
+      compTwoHand+= getCard(compTwoHand) + " ";
+      compTwoHand += getCard(compTwoHand) + " ";
+      compTwoHand += getCard(compTwoHand) + " ";
+      compTwoHand += getCard(compTwoHand) + " ";
+      System.out.println(compTwoHand);
+      
+      int p1Points = 0, c1Points = 0, c2Points = 0;
+      while (!gameOver(p1Points, c1Points, c2Points)) {
+        String result = playRound();
+        int firstDash = result.indexOf("-");
+        int secondDash = result.indexOf("-", firstDash + 1);
+        p1Points += Integer.parseInt(result.substring(0, firstDash));
+        c1Points += Integer.parseInt(result.substring(firstDash + 1, secondDash));
+        c2Points += Integer.parseInt(result.substring(secondDash + 1));
+        System.out.println("Current Score: " + p1Points + " " + c1Points + " " + c2Points);
     }
+  }
 
     private static String getCard(String usedCards) {
-            String card = getFace() + getSuit();
-      
-            while (usedCards.indexOf(card) >= 0) {
-               card = getFace() + getSuit();
-            }
-            return card;
-          }
+      String card = getFace() + getSuit();
+      while (usedCards.indexOf(card) >= 0) {
+        card = getFace() + getSuit();
+      }
+       return card;
+    }
           
     private static String getFace() {
-            int suit = (int) (Math.random() * CARDS_PER_SUIT);
-            if (suit >= 2 && suit <= 10){
-              return suit + "";
-            }else if (suit == 1){
-              return ACE;
-            }else if (suit == 11){
-              return JACK;
-            }else if (suit == 12){
-              return QUEEN;
-            }else{
-              return KING;
-            }
-          }
+      int suit = (int) (Math.random() * CARDS_PER_SUIT);
+      if (suit >= 2 && suit <= 10){
+      return suit + "";
+     }else if (suit == 1){
+      return ACE;
+     }else if (suit == 11){
+      return JACK;
+     }else if (suit == 12){
+      return QUEEN;
+     }else{
+      return KING;
+     }
+    }
 
     private static String getSuit() {
-            int suit = (int) (Math.random() * NUM_SUITS);
-      
-            if (suit == 0)
-               return HEARTS;
-            else if (suit == 1)
-               return DIAMONDS;
-            else if (suit == 2)
-               return CLUBS;
-            else
-               return SPADES;
-        }
-      
+      int suit = (int) (Math.random() * NUM_SUITS);
+      if (suit == 0)
+      return HEARTS;
+      else if (suit == 1)
+      return DIAMONDS;
+      else if (suit == 2)
+      return CLUBS;
+      else
+      return SPADES;
+    }
+        
+   private static String playRound() {
+
+    String playerHand = "";
+    String c1Hand = "";
+    String c2Hand = "";
+
+    String topCard = ""; // no eights
+
+    // "7H 3D AC JS-9D"
+    String temp = processPlayer(playerHand, topCard);
+    playerHand = temp.substring(0, temp.indexOf("-"));
+    topCard = temp.substring(temp.indexOf("-") + 1);
+    temp = processComputer(c1Hand, topCard);
+    c1Hand = temp.substring(0, temp.indexOf("-"));
+    topCard = temp.substring(temp.indexOf("-") + 1);
+    temp = processComputer(c2Hand, topCard);
+    c2Hand = temp.substring(0, temp.indexOf("-"));
+    topCard = temp.substring(temp.indexOf("-") + 1);
+
+    return "37-0-12";
+  }
+
+ private static String processComputer(String c1Hand, String topCard) {
+   return "-4D";
+}
+
+ private static String processPlayer(String playerHand, String topCard) {
+    return "7H 3D 4C-2D";
+ }
+
+ private static boolean gameOver(int p1Points, int c1Points, int c2Points) {
+   return p1Points >= 100 || c1Points >= 100 || c2Points >= 100;
+ }
+ 
+ 
+     
+        
 }
